9459f5334d2451a75f769f551371e656f63be0b3
Sviatoslav Kashchin <skashchin@gmail.com>
2019-04-14 19:32:38 +0300

    added KafkaConsumer config; code refactor

--- src/main/java/com/sk/bigdata/config/KafkaConfiguration.java
+++ src/main/java/com/sk/bigdata/config/KafkaConfiguration.java
@@ -1,28 +1,30 @@
 package com.sk.bigdata.config;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
 import java.util.Properties;
 
 @Configuration
 public class KafkaConfiguration {
-    public static final String KAFKA_BROKERS = "localhost:9092";
-    public static final Integer MESSAGE_COUNT_PER_SEC = 100000;
-    public static final String CLIENT_ID = "ss_producer_1";
-    public static final String TOPIC_NAME = "sk_bigdata_twitter";
+    public static final String TOPIC_NAME = "sk_bigdata_twitter_twits";
 
-    public static final String GROUP_ID_CONFIG = "consumerGroup1";
-    public static final Integer MAX_NO_MESSAGE_FOUND_COUNT = 100;
-    public static final String OFFSET_RESET_EARLIER = "earliest";
+    private static final String KAFKA_BROKERS = "localhost:9092";
+    private static final String CLIENT_ID = "sk_bigdata_producer_1";
 
-    public static final Integer POLL_RATE_SEC = 1;
-    public static final Integer MAX_POLL_RECORDS = 10000;
-    public static final Integer BATCH_SIZE_BYTES = 1024 * 1000;
+    private static final String GROUP_ID_CONFIG = "consumer_group_1";
+    private static final String OFFSET_RESET_EARLIER = "earliest";
+
+    private static final Integer MAX_POLL_RECORDS = 10000;
 
     @Bean
     public Producer<String, String> producer() {
@@ -31,8 +33,24 @@ public class KafkaConfiguration {
         props.put(ProducerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.BATCH_SIZE_CONFIG, BATCH_SIZE_BYTES);
 
         return new KafkaProducer<>(props);
+    }
+
+    @Bean
+    public Consumer<String, String> kafkaConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID_CONFIG);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, MAX_POLL_RECORDS);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, OFFSET_RESET_EARLIER);
+
+        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+
+        return consumer;
     }
 }
